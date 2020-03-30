2f2ac2faaf224d42092ca6148611a5134c5cfb38
Arnaud Drain <drain.arnaud@hotmail.fr>
2020-03-31 00:10:07 +0200

    Complete and simplify MusicXML parser

--- src/parsers/MusicXML.js
+++ src/parsers/MusicXML.js
@@ -1,27 +1,49 @@
+function getTextValue(domElement, tagName) {
+  return domElement.getElementsByTagName(tagName)[0].childNodes[0].nodeValue;
+}
+
+function parseNote(noteXML) {
+  return {
+    pitch: {
+      step: getTextValue(noteXML, 'step'),
+      octave: getTextValue(noteXML, 'octave')
+    },
+    duration: getTextValue(noteXML, 'duration'),
+    type: getTextValue(noteXML, 'type')
+  };
+}
+
+function parseMeasure(measureXML) {
+  return {
+    number: measureXML.getAttribute('number'),
+    time: {
+      beats: getTextValue(measureXML, 'beats'),
+      beatType: getTextValue(measureXML, 'beat-type')
+    },
+    clef: {
+      sign: getTextValue(measureXML, 'sign'),
+      line: getTextValue(measureXML, 'line')
+    },
+    notes: [...measureXML.getElementsByTagName('note')].map(n => parseNote(n))
+  };
+}
+
+function parsePart(partXML) {
+  return {
+    id: partXML.id,
+    measures: [...partXML.getElementsByTagName('measure')].map(m => parseMeasure(m))
+  };
+}
+
 async function parse(fileName) {
   const response = await fetch(fileName);
   const text = await response.text();
   const dataXML = new window.DOMParser().parseFromString(text, 'text/xml');
 
-  const music = {};
-
-  music.name = dataXML.getElementsByTagName('part-name')[0].childNodes[0].nodeValue;
-
-  music.parts = [];
-  const partsXML = dataXML.getElementsByTagName('part');
-  for (const partXML of partsXML) {
-    const part = {};
-    part.id = partXML.id;
-    part.measures = [];
-    const measuresXML = partXML.getElementsByTagName('measure');
-    for (const measureXML of measuresXML) {
-      const measure = {};
-      measure.number = measureXML.getAttribute('number');
-      part.measures.push(measure);
-    }
-    music.parts.push(part);
-  }
-  return music;
+  return {
+    name: getTextValue(dataXML, 'part-name'),
+    parts: [...dataXML.getElementsByTagName('part')].map(p => parsePart(p))
+  };
 }
 
 export { parse }
