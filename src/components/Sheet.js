055250bffb8293df6ae90795c65bdecfc2f99388
Arnaud Drain <drain.arnaud@hotmail.fr>
2020-04-12 20:35:16 +0200

    More accurate height of page

--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -4,7 +4,7 @@ import Line from './Line'
 class Sheet extends React.Component {
   render() {
     const nbLines = Math.ceil(this.props.music.parts[0].measures.length / 3);
-    const height = nbLines * 380;
+    const height = nbLines * 310;
     let lines = [...Array(nbLines).keys()].map((l, i) => <Line number={i} key={i} music={this.props.music} />);
     return <>
       <h1>{this.props.music.name}</h1>
